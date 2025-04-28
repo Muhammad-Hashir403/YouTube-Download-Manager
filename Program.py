2a24dd93d26cd16e21d582096355851b338a0011
Muhammad <m.hashir403a@gmail.com>
2025-04-28 08:24:35 +0500

    Adding project files

---
+++ Program.py
@@ -0,0 +1,122 @@
+# print("Hello Hashir")
+# v=1
+# print("value of v is =",v)
+# v=v+1
+# print("new value of v is =",v)
+# v=v*5
+# print("After multiplying =",v)
+# print("By dividing =",v/5)
+
+
+
+# todo: ------String---------------
+
+# word1= "Good"
+# word2 = "Morning"
+# word3="to you too !"
+#
+# print(word1,word2)
+# sentence= word1 +" "+ word2 +" "+  word3
+# print(sentence)
+
+'''
+lasdsladfdsalfnlskdalkdfasfasfa
+fa
+fa
+f
+adf
+af
+a
+dgf
+'''
+
+# todo: --------------------Syntax--------------------
+
+# print("Enter your name")
+# name=input()
+# print("Entre your age")
+# age=int(input())
+# if (age <=40):
+#     print(name," you are under age :"+str(age))
+#
+# else:
+#     print(name," you are over age:  "+str(age))
+
+# counter = "junaid"
+# print(counter)
+#
+# del counter
+# print(str(counter))
+
+# a="kajsf"
+# b=12;
+# print(f'a= {type(a)}    and b ={type(b)} ')
+
+
+# todo:----------------------- LAb TASK 1----------------------------
+# Meanu = """
+# 1- soup and salad
+# 2- pasta with meat sauce
+# 3- chef's special
+#
+# Which number would you like to order ?
+# """
+# print(str(Meanu))
+# b=int(input())
+# if (b==1):
+#     print("soup and salad")
+# elif (b==2):
+#     print("pasta with meat sauce")
+# elif (b==3):
+#     print("chef's special")
+# else:
+#     print("sorry , that is not valid choice")
+
+# todo:----------------------- LAb TASK 2----------------------------
+
+# John = 3
+# Marry = 5
+# Adam = 6
+# print( "John ," "Marry," "Adam")
+# sum = John+Marry+Adam
+# print(f'Total Apples are =  {sum}')
+# print(John , Marry , Adam)
+# Total_Apple= John+Marry+Adam
+#
+# if(Total_Apple==10):
+#     print("Apple is  eequal to  10")
+# elif(Total_Apple>10):
+#     print("Apple is  greater than  10")
+# else:
+#     print("Apple is  less then  10")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
